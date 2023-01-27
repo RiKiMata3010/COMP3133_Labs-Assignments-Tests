f27c2ebb00ec463b3624c4d29cef8bd6c51330b3
Phoenix Armand Ani <PhoenixArmand30@outlook.com>
2023-01-27 18:00:06 -0500

    almost done

---
+++ Lab02_101336759_COMP3133/test/calculator.js
@@ -0,0 +1,46 @@
+const assert = require('chai').assert;
+const add = require ('../app').add
+const mul = require ('../app').mul
+const div = require ('../app').div
+const sub = require ('../app').sub
+
+describe('Addition', function(){
+    it('function should add values', function(){
+        let result = add(7, 7);
+        assert.equal(result, 14)
+    })
+    it('function should add values', function(){
+        let result = add(7, 7);
+        assert.equal(result, 13)
+    })
+})
+describe('Subtraction', function(){
+    it('function should subtract values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 0)
+    })
+    it('function should add values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 1)
+    })
+})
+describe('Multiplication', function(){
+    it('function should subtract values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 0)
+    })
+    it('function should add values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 1)
+    })
+})
+describe('Subtraction', function(){
+    it('function should subtract values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 0)
+    })
+    it('function should add values', function(){
+        let result = sub(7, 7);
+        assert.equal(result, 1)
+    })
+})
